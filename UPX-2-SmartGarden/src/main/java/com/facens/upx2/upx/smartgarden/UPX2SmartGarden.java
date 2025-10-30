5791fb07bef9643f3e76ee1c4e376c595f048a9e
GuRosendo <gustavorosendowork@gmail.com>
2025-10-29 22:35:50 -0300

    :sparkles: Feat: Primeiro commit do projeto JAVA

---
+++ UPX-2-SmartGarden/src/main/java/com/facens/upx2/upx/smartgarden/UPX2SmartGarden.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.facens.upx2.upx.smartgarden;
+
+/**
+ *
+ * @author gusta
+ */
+public class UPX2SmartGarden {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
